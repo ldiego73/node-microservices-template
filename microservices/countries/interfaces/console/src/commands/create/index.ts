86d044a78100aaae92e864e92539dace7b1223a6
Luis Diego <lfdiego7@gmail.com>
2020-05-27 17:30:00 -0500

    fix: eslint into interfaces console

--- microservices/countries/interfaces/console/src/commands/create/index.ts
+++ microservices/countries/interfaces/console/src/commands/create/index.ts
@@ -1,8 +1,6 @@
 import { createCountryUseCase } from '@micro/countries-core';
 import { CreateCommand } from './create.command';
 
-const createCommand = new CreateCommand(
-  createCountryUseCase
-).create();
+const createCommand = new CreateCommand(createCountryUseCase).create();
 
 export { createCommand };
--- microservices/countries/interfaces/console/src/commands/delete/index.ts
+++ microservices/countries/interfaces/console/src/commands/delete/index.ts
@@ -1,8 +1,6 @@
 import { deleteCountryUseCase } from '@micro/countries-core/lib/application/use-cases/delete-country';
 import { DeleteCommand } from './delete.command';
 
-const deleteCommand = new DeleteCommand(
-  deleteCountryUseCase
-).create();
+const deleteCommand = new DeleteCommand(deleteCountryUseCase).create();
 
 export { deleteCommand };
