2ecbdb402fd4ac62c0d52a9a1e4751d94e56f968
Luis Diego <lfdiego7@gmail.com>
2020-06-10 00:08:22 -0500

    yarn: update countries app

--- microservices/countries/app/src/index.ts
+++ microservices/countries/app/src/index.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-import { MicroApplication } from '@micro/kernel';
-import { CountryConsoleApplication } from '@micro/countries-console';
-import { CountryGraphqlApplication } from '@micro/countries-graphql';
-import { CountryRestApplication } from '@micro/countries-rest';
+import { CountryConsoleApplication } from "@micro/countries-console";
+import { CountryGraphqlApplication } from "@micro/countries-graphql";
+import { CountryRestApplication } from "@micro/countries-rest";
+import { MicroApplication } from "@micro/kernel";
 
 const app: MicroApplication = new CountryRestApplication();
 
